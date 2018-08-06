75f596a40bc6cedc60bbce6f66154363a990c999
Steven Herbst <sherbst@stanford.edu>
2018-08-05 17:43:52 -0700

    initial checkin

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msdsl',
+    version='0.0.1',
+    description='Library for describing mixed-signal circuits for emulation',
+    url='https://github.com/sgherbst/msdsl',
+    author='Steven Herbst',
+    author_email='sherbst@stanford.edu',
+    packages=['msdsl'],
+    install_requires=[
+        'scipy',
+        'numpy'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)

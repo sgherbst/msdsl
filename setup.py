9b4975916f0472df39700250669fa69c2c03c231
Steven Herbst <sherbst@stanford.edu>
2020-03-18 16:50:12 -0700

    fix issue with multiple instantiations of functions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'msdsl'
-version = '0.1.9'
+version = '0.2.0'
 
 DESCRIPTION = '''\
 Library for generating synthesizable mixed-signal models for FPGA emulation\

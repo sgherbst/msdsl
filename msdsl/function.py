fb54606a63b2e8e3d72b18d421a1a4bd197f2dad
Steven Herbst <sherbst@stanford.edu>
2020-07-21 10:47:40 -0700

    refactor common methods into GeneralFunction

--- msdsl/function.py
+++ msdsl/function.py
@@ -6,84 +6,13 @@ from .expr.format import RealFormat
 
 class GeneralFunction:
     def __init__(self, domain, name='real_func', numel=512, order=0,
-                 clamp=True, coeff_widths=None, coeff_exps=None):
+                 clamp=True, coeff_widths=None, coeff_exps=None,
+                 verif_per_seg=10, strategy=None):
         # set defaults
         if coeff_widths is None:
             coeff_widths = [18]*(order+1)
         if coeff_exps is None:
             coeff_exps = [None]*(order+1)
-
-        self.domain = domain
-        self.name = name
-        self.numel = numel
-        self.order = order
-        self.clamp = clamp
-        self.coeff_widths = coeff_widths
-        self.coeff_exps = coeff_exps
-
-    @property
-    def addr_bits(self):
-        return int(ceil(log2(self.numel)))
-
-class PlaceholderFunction(GeneralFunction):
-    def __init__(self, domain, name='real_func', numel=512, order=0,
-                 clamp=True, coeff_ranges=None, coeff_exps=None,
-                 coeff_widths=None):
-        # set default for coefficient widths
-        if coeff_widths is None:
-            if coeff_exps is None:
-                coeff_widths = [18]*(order+1)
-            else:
-                coeff_widths = [self.calc_width(range_, exponent)
-                                for range_, exponent in zip(coeff_ranges, coeff_widths)]
-
-        # set default for coefficient exponents
-        if coeff_exps is None:
-            coeff_exps = [self.calc_exponent(range_, width)
-                          for range_, width in zip(coeff_ranges, coeff_widths)]
-
-        # set default values for coefficient ranges
-        if coeff_ranges is None:
-            coeff_ranges = [self.calc_range(width, exponent)
-                            for width, exponent in zip(coeff_widths, coeff_exps)]
-
-        # save formatting information
-        self.formats = [RealFormat(range_=range_, width=width, exponent=exponent)
-                        for range_, width, exponent in zip(coeff_ranges, coeff_widths, coeff_exps)]
-
-        # call super constructor
-        super().__init__(domain=domain, name=name, numel=numel, order=order,
-                         clamp=clamp, coeff_widths=coeff_widths,
-                         coeff_exps=coeff_exps)
-
-    @staticmethod
-    def calc_exponent(range, width):
-        if range == 0:
-            return 0
-        else:
-            return int(ceil(log2(range/(2**(width-1)-1))))
-
-    @staticmethod
-    def calc_width(range, exponent):
-        if range == 0:
-            return 1
-        else:
-            return int(ceil(1+log2((range/(2**exponent))+1)))
-
-    @staticmethod
-    def calc_range(width, exponent):
-        return 2**(width+exponent-1)
-
-class Function(GeneralFunction):
-    def __init__(self, func, domain, name='real_func', dir='.',
-                 numel=512, order=0, clamp=True, coeff_widths=None,
-                 coeff_exps=None, verif_per_seg=10, strategy=None):
-        # call super constructor
-        super().__init__(domain=domain, name=name, numel=numel, order=order,
-                         clamp=clamp, coeff_widths=coeff_widths,
-                         coeff_exps=coeff_exps)
-
-        # set defaults
         if strategy is None:
             if order in {0, 1}:
                 strategy = 'spline'
@@ -91,24 +20,29 @@ class Function(GeneralFunction):
                 strategy = 'cvxpy'
 
         # save settings
-        self.func = func
-        self.dir = dir
+        self.domain = domain
+        self.name = name
+        self.numel = numel
+        self.order = order
+        self.clamp = clamp
+        self.coeff_widths = coeff_widths
+        self.coeff_exps = coeff_exps
         self.verif_per_seg = verif_per_seg
         self.strategy = strategy
 
-        # initialize variables
-        self.tables = None
-        self.create_tables()
+    @property
+    def addr_bits(self):
+        return int(ceil(log2(self.numel)))
 
-    def create_tables(self):
+    def get_coeffs(self, func):
         if self.strategy == 'cvxpy':
-            self.create_tables_cvxpy()
+            return self.get_coeffs_cvxpy(func)
         elif self.strategy == 'spline':
-            self.create_tables_spline()
+            return self.get_coeffs_spline(func)
         else:
             raise Exception(f'Unknown strategy: {self.strategy}')
 
-    def create_tables_cvxpy(self):
+    def get_coeffs_cvxpy(self, func):
         # load cvxpy module
         try:
             import cvxpy as cp
@@ -122,7 +56,7 @@ class Function(GeneralFunction):
         x_vec = x_vec + np.random.uniform(0, lsb, x_vec.shape)
 
         # evaluate the function at the sample poitns
-        y_vec = self.func(x_vec)
+        y_vec = func(x_vec)
 
         # create vectors of integer and fractional addresses
         addr_real = (x_vec - self.domain[0])*((self.numel-1)/(self.domain[1]-self.domain[0]))
@@ -169,54 +103,128 @@ class Function(GeneralFunction):
         prob = cp.Problem(cp.Minimize(cp.sum_squares(cost)), eqn_cons)
         prob.solve()
 
-        # unpack solution into tables
-        self.tables = []
-        for k in range(self.order+1):
-            name = f'{self.name}_lut_{k}'
-            vals = coeffs[k].value
-            table = RealTable(vals=vals, width=self.coeff_widths[k],
-                              exp=self.coeff_exps[k], name=name,
-                              dir=self.dir)
-            self.tables.append(table)
+        # unpack solution
+        return [coeffs[k].value for k in range(self.order+1)]
 
-    def create_tables_spline(self):
+    def get_coeffs_spline(self, func):
         # sample the function
         x_vec = np.linspace(self.domain[0], self.domain[1], self.numel)
-        y_vec = self.func(x_vec)
+        y_vec = func(x_vec)
 
-        # create the tables
-        self.tables = []
-        for k in range(self.order+1):
-            # name the table
-            name = f'{self.name}_lut_{k}'
+        # create the coefficient vectors
+        retval = []
+        if self.order >= 0:
+            retval.append(y_vec[:])
+        if self.order >= 1:
+            retval.append(np.concatenate((np.diff(y_vec), [0])))
+        if self.order >= 2:
+            raise Exception('The spline method only supports order=0 and order=1 for now.')
 
-            # compute table values
-            # TODO: make this more generic
-            if k == 0:
-                vals = y_vec[:]
-            elif k == 1:
-                vals = np.concatenate((np.diff(y_vec), [0]))
-            else:
-                raise Exception('Only order=0 and order=1 are supported for now.')
+        # return the coefficient vector
+        return retval
 
-            # convert to a synthesizable table
-            table = RealTable(vals=vals, width=self.coeff_widths[k], exp=self.coeff_exps[k],
-                              name=name, dir=self.dir)
-
-            # add table to the list of tables
-            self.tables.append(table)
-
-    def eval_on(self, samp):
+    def eval_on(self, samp, coeffs):
         # calculate address as a real value
         addr_real = (samp - self.domain[0])*((self.numel-1)/(self.domain[1]-self.domain[0]))
         if self.clamp:
             addr_real = np.clip(addr_real, 0, self.numel-1)
+
         # calculate integer and fractional addresses
         addr_int = addr_real.astype(np.int)
         addr_frac = addr_real - addr_int
+
         # sum up output contributions
         out = np.zeros(len(samp))
         for k in range(self.order+1):
-            out += self.tables[k].vals[addr_int] * np.power(addr_frac, k)
+            out += coeffs[k][addr_int] * np.power(addr_frac, k)
+
         # return output
         return out
+
+class PlaceholderFunction(GeneralFunction):
+    def __init__(self, domain, name='real_func', numel=512, order=0,
+                 clamp=True, coeff_ranges=None, coeff_exps=None,
+                 coeff_widths=None, verif_per_seg=10, strategy=None):
+        # set default for coefficient widths
+        if coeff_widths is None:
+            if coeff_exps is None:
+                coeff_widths = [18]*(order+1)
+            else:
+                coeff_widths = [self.calc_width(range_, exponent)
+                                for range_, exponent in zip(coeff_ranges, coeff_widths)]
+
+        # set default for coefficient exponents
+        if coeff_exps is None:
+            coeff_exps = [self.calc_exponent(range_, width)
+                          for range_, width in zip(coeff_ranges, coeff_widths)]
+
+        # set default values for coefficient ranges
+        if coeff_ranges is None:
+            coeff_ranges = [self.calc_range(width, exponent)
+                            for width, exponent in zip(coeff_widths, coeff_exps)]
+
+        # save formatting information
+        self.formats = [RealFormat(range_=range_, width=width, exponent=exponent)
+                        for range_, width, exponent in zip(coeff_ranges, coeff_widths, coeff_exps)]
+
+        # call super constructor
+        super().__init__(domain=domain, name=name, numel=numel, order=order,
+                         clamp=clamp, coeff_widths=coeff_widths,
+                         coeff_exps=coeff_exps, verif_per_seg=verif_per_seg,
+                         strategy=strategy)
+
+    @staticmethod
+    def calc_exponent(range, width):
+        if range == 0:
+            return 0
+        else:
+            return int(ceil(log2(range/(2**(width-1)-1))))
+
+    @staticmethod
+    def calc_width(range, exponent):
+        if range == 0:
+            return 1
+        else:
+            return int(ceil(1+log2((range/(2**exponent))+1)))
+
+    @staticmethod
+    def calc_range(width, exponent):
+        return 2**(width+exponent-1)
+
+class Function(GeneralFunction):
+    def __init__(self, func, domain, name='real_func', dir='.',
+                 numel=512, order=0, clamp=True, coeff_widths=None,
+                 coeff_exps=None, verif_per_seg=10, strategy=None):
+        # call super constructor
+        super().__init__(domain=domain, name=name, numel=numel, order=order,
+                         clamp=clamp, coeff_widths=coeff_widths,
+                         coeff_exps=coeff_exps, verif_per_seg=verif_per_seg,
+                         strategy=strategy)
+
+        # save settings
+        self.func = func
+        self.dir = dir
+
+        # initialize variables
+        self.tables = None
+        self.create_tables()
+
+    def create_tables(self):
+        # calculate coefficients
+        coeffs = self.get_coeffs(self.func)
+
+        # write coefficients in tables
+        self.tables = []
+        for k, coeff_vec in enumerate(coeffs):
+            name = f'{self.name}_lut_{k}'
+            table = RealTable(vals=coeff_vec, width=self.coeff_widths[k],
+                              exp=self.coeff_exps[k], name=name, dir=self.dir)
+            self.tables.append(table)
+
+    def eval_on(self, samp, coeffs=None):
+        # set defaults
+        if coeffs is None:
+            coeffs = [self.tables[k].vals for k in range(self.order+1)]
+
+        # call the parent method
+        return super().eval_on(samp=samp, coeffs=coeffs)

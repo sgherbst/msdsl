34ba3d828db40bb00daae4c50467152548f340b1
Steven Herbst <sherbst@stanford.edu>
2020-08-18 15:55:49 -0700

    fix synthesis corner case for real-number constants

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = 'msdsl'
-version = '0.3.2.dev1'
+version = '0.3.2.dev2'
 
 DESCRIPTION = '''\
 Library for generating synthesizable mixed-signal models for FPGA emulation\

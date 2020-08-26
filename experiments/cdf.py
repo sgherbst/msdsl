49bc11abbd28adc04e3da4c91307699e35405291
Steven Herbst <sherbst@stanford.edu>
2020-08-26 14:19:13 -0700

    updates to experiments

--- experiments/cdf.py
+++ experiments/cdf.py
@@ -1,13 +1,13 @@
 # results:
 
 # using np.random.normal
-# with 100,000,000 pts: can verify 5 sigma to 5%, 3/4 sigma to 1%, 2 sigma and below to 0.1%
-# with 10,000,000 pts: can verify 4 sigma to 5%, 3 sigma to 2%, 2 sigma to 0.5%, 1 sigma to 0.1%
-# with 1,000,000 pts: can verify 3 sigma to 5%, 2/1 sigma to 1%
-# with 100,000 pts: can verify 2 sigma to 2%, 1 sigma to 1%
+# with 100000000 pts: can verify 5 sigma to 5%, 3/4 sigma to 1%, 2 sigma and below to 0.1%
+# with 10000000 pts: can verify 4 sigma to 5%, 3 sigma to 2%, 2 sigma to 0.5%, 1 sigma to 0.1%
+# with 1000000 pts: can verify 3 sigma to 5%, 2/1 sigma to 1%
+# with 100000 pts: can verify 2 sigma to 2%, 1 sigma to 1%
 
 # using Xcelium simulation $dist_normal(seed, 0, 10000)/10000.0
-# with 1,000,000,000: can verify 5/6 sigma to 5%, 4 sigma and below to better than 1%
+# with 1000000000: can verify 5/6 sigma to 5%, 4 sigma and below to better than 1%
 
 # With msdsl, 1/2 sigma are good to within 1%, but the error rate is already off by 36% at 3 sigma
 # 4 sigma and beyond are very far off
@@ -21,13 +21,31 @@ N_PTS = 100000000
 # data = np.random.normal(size=N_PTS)
 
 # using msdsl
-from scipy.stats import truncnorm
+# mapping and inverse mapping function
+from scipy.stats import norm
 from msdsl import Function
-inv_cdf = lambda x: truncnorm.ppf(x, -10, +10)
-func = Function(inv_cdf, domain=[0.0, 0.5], order=1, numel=512, log_bits=7)
-unf_noise = np.random.uniform(-0.5, 0.5, N_PTS)
-unf_noise_sgn = np.sign(unf_noise)
-data = func.eval_on(np.abs(unf_noise)) * unf_noise_sgn
+
+num_bits = 31
+
+# logarithmic mapping
+def map_f(r):
+    x = np.floor(np.log2(r))
+    y = (r/(2.0**x)) - 1
+    return x + y
+def unmap_f(r):
+    x = np.floor(r)
+    return (2.0**(x-num_bits-1)) * (1 + r - x)
+
+# calculate domain
+min_val = map_f(1)
+max_val = map_f((1<<num_bits)-1)
+domain = [min_val, max_val]
+
+func = Function(lambda x: norm.ppf(unmap_f(x)), domain=domain, order=1, numel=512)
+unf_noise = np.random.randint(1, 1<<(num_bits+1), N_PTS)
+unf_noise_sgn = 2*(((unf_noise>>num_bits) & 1) - 0.5)
+unf_noise_mag = unf_noise & ((1<<num_bits)-1)
+data = func.eval_on(map_f(unf_noise_mag)) * unf_noise_sgn
 
 # construct emperical CDF
 data_sorted = np.sort(data)
--- experiments/gaussian.py
+++ experiments/gaussian.py
@@ -1,17 +1,40 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from msdsl import Function
-from scipy.stats import truncnorm
+from scipy.stats import norm
 
-num_sigma = 6
-inv_cdf = lambda x: truncnorm.ppf(x, -num_sigma, +num_sigma)
+inv_cdf = norm.ppf
+num_bits = 31  # 31 because the top bit is used to determine the sign
 
 num_test = 10000
-test_pts = 2**(np.random.uniform(-32, -1, num_test))
+test_pts = 2**(np.random.uniform(0, num_bits, num_test))
+test_pts = np.floor(test_pts).astype(np.int)
+test_pts = np.sort(test_pts)
 
-f = Function(inv_cdf, domain=[0, 0.5], numel=1024, order=1)
-meas = f.eval_on(test_pts)
-expct = inv_cdf(test_pts)
+# logarithmic mapping
+def map_f(r):
+    x = np.floor(np.log2(r))
+    y = (r/(2.0**x)) - 1
+    return x + y
+def unmap_f(r):
+    x = np.floor(r)
+    return (2.0**(x-num_bits-1)) * (1 + r - x)
+
+# calculate domain
+min_val = map_f(1)
+max_val = map_f((1<<num_bits)-1)
+domain = [min_val, max_val]
+print(f'domain={domain}')
+
+func = Function(lambda x: inv_cdf(unmap_f(x)), domain=domain, order=1, numel=512)
+meas = func.eval_on(map_f(test_pts))
+expct = inv_cdf(test_pts/(1<<(num_bits+1)))
 err = meas - expct
 
 idx = np.argmax(np.abs(err))
 print(f'Worst error: {err[idx]} @ {test_pts[idx]}')
+
+plt.semilogx(test_pts, meas)
+plt.semilogx(test_pts, expct)
+plt.legend(['meas', 'expct'])
+plt.show()

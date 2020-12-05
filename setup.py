7cea1bffb076875dc4c4eabea61e121ecc148f1d
Steven Herbst <sherbst@stanford.edu>
2020-12-04 20:39:19 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = 'msdsl'
-version = '0.3.4'
+version = '0.3.5'
 
 DESCRIPTION = '''\
 Library for generating synthesizable mixed-signal models for FPGA emulation\

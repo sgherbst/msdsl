b480603d35673a0f8b22d993a24ccab52dd90a75
Steven Herbst <sherbst@stanford.edu>
2021-03-02 17:02:26 -0800

    created a template for saturation

---
+++ msdsl/templates/saturation.py
@@ -0,0 +1,26 @@
+from msdsl import MixedSignalModel
+from msdsl.interp.nonlin import calc_tanh_vsat, tanhsat
+
+class Saturation(MixedSignalModel):
+    def __init__(self, *args, compr=-1, units='dB', veval=1.0, in_='in_',
+                 out='out', domain=None, order=1, numel=64, **kwargs):
+        # call the super constructor
+        super().__init__(*args, **kwargs)
+
+        # set defaults
+        if domain is None:
+            domain = [-2*abs(veval), +2*abs(veval)]
+
+        # find vsat
+        self.vsat = calc_tanh_vsat(compr=compr, units=units)
+
+        # create IOs
+        self.add_analog_input(in_)
+        self.add_analog_output(out)
+
+        # create and apply function
+        real_func = self.make_function(self.func, domain=domain, order=order, numel=numel, write_tables=False)
+        self.set_from_func(self.get_signal(out), real_func, self.get_signal(in_), func_mode='async')
+
+    def func(self, v):
+        return tanhsat(v, self.vsat)
--- tests/tanh/test_tanh.py
+++ tests/tanh/test_tanh.py
@@ -7,40 +7,22 @@ import magma as m
 
 # msdsl imports
 from ..common import *
-from msdsl import MixedSignalModel, VerilogGenerator
-from msdsl.interp.nonlin import calc_tanh_vsat, tanhsat
+from msdsl import VerilogGenerator
+from msdsl.templates.saturation import Saturation
 
 BUILD_DIR = Path(__file__).resolve().parent / 'build'
-VSAT = calc_tanh_vsat(-1, 'dB')
-
-def myfunc(v):
-    return tanhsat(v, VSAT)
 
 def pytest_generate_tests(metafunc):
     pytest_sim_params(metafunc)
     pytest_real_type_params(metafunc)
-
-def gen_model(order=1, numel=64, real_type=RealType.FixedPoint):
-    # create mixed-signal model
-    model = MixedSignalModel('model', build_dir=BUILD_DIR, real_type=real_type)
-    model.add_analog_input('in_')
-    model.add_analog_output('out')
-
-    # create function
-    real_func = model.make_function(myfunc, domain=[-2.0, +2.0], order=order, numel=numel, write_tables=False)
-
-    # apply function
-    model.set_from_func(model.out, real_func, model.in_, func_mode='async')
-
-    # write the model
-    return model.compile_to_file(VerilogGenerator())
 
 def test_tanh(simulator, real_type, err_lim=2.5e-4):
     # set the random seed for repeatable results
     np.random.seed(0)
 
     # generate model
-    model_file = gen_model(real_type=real_type)
+    model = Saturation('model', build_dir=BUILD_DIR, real_type=real_type)
+    model_file = model.compile_to_file(VerilogGenerator())
 
     # declare circuit
     class dut(m.Circuit):
@@ -52,10 +34,6 @@ def test_tanh(simulator, real_type, err_lim=2.5e-4):
 
     # create the tester
     tester = MsdslTester(dut)
-
-    # initialize
-    tester.poke(dut.in_, 0)
-    tester.eval()
 
     # save the outputs
     inpts = np.random.uniform(-2, +2, 100)
@@ -82,7 +60,7 @@ def test_tanh(simulator, real_type, err_lim=2.5e-4):
     apprx = np.array([elem.value for elem in apprx], dtype=float)
 
     # compute the exact response to inputs
-    exact = myfunc(inpts)
+    exact = model.func(inpts)
 
     # check the result
     err = np.sqrt(np.mean((exact-apprx)**2))

2bb300467ba494095f159d83d5061fb3ff3c1415
Steven Herbst <sherbst@stanford.edu>
2021-03-08 22:58:47 -0800

    fix oscillator overflow issue found with vivado

--- msdsl/templates/oscillator.py
+++ msdsl/templates/oscillator.py
@@ -1,4 +1,5 @@
 from msdsl import MixedSignalModel, to_sint, to_uint, clamp_op
+from msdsl.expr.format import SIntFormat
 from msdsl.expr.extras import if_
 
 class OscillatorModel(MixedSignalModel):
@@ -21,14 +22,22 @@ class OscillatorModel(MixedSignalModel):
         self.set_this_cycle(clk_en, self.dt_req == self.emu_dt)
 
         # discretize the period to a uint
+        # TODO: cleanup
         period_real = self.set_this_cycle(
             'period_real', period/dt_scale)
         period_sint = self.set_this_cycle(
-            'period_sint', clamp_op(to_sint(period_real, width=dt_width+1), 0, (1<<dt_width)-1))
+            'period_sint', to_sint(period_real, width=dt_width+1))
+        period_sint.format_ = SIntFormat(
+            width=dt_width+1, min_val=0, max_val=((1<<dt_width)-1))
         period_uint = self.set_this_cycle(
             'period_uint', to_uint(period_sint, width=dt_width))
 
         # update the timestep request
-        dt_req_decr = self.set_this_cycle('dt_req_decr', to_uint(clamp_op(dt_req-emu_dt, 0, (1<<dt_width)-1)))
-        dt_req_imm = self.set_this_cycle('dt_req_imm', if_(clk_en, period_uint, dt_req_decr))
+        # TODO: cleanup
+        dt_req_decr_sint = self.set_this_cycle('dt_req_decr_sint', dt_req-emu_dt)
+        dt_req_decr_sint.format_ = SIntFormat(
+            width=dt_width+1, min_val=0, max_val=((1<<dt_width)-1))
+        dt_req_decr_uint = self.set_this_cycle(
+            'dt_req_decr_uint', to_uint(dt_req_decr_sint, width=dt_width))
+        dt_req_imm = self.set_this_cycle('dt_req_imm', if_(clk_en, period_uint, dt_req_decr_uint))
         self.set_next_cycle(dt_req, dt_req_imm, clk=clk, rst=rst)
